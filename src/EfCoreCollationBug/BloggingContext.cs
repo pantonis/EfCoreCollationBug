0ff6286d240ee44b9ed59a4bbe2a1217e85335bb
pantonis <antonischa@gmail.com>
2024-11-11 11:40:52 +0200

    Configure varchar columns to use custom collation

--- src/EfCoreCollationBug/BloggingContext.cs
+++ src/EfCoreCollationBug/BloggingContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,23 +10,28 @@ namespace EfCoreCollationBug
 {
     public class BloggingContext : DbContext
     {
+        string collName = """en_US.utf8_ci""";
+
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
-
-
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            string collName = """en_US.utf8_ci""";
-            
             modelBuilder.HasCollation(name: collName, locale: "en_US.utf8", provider: "icu", deterministic: false);
-            modelBuilder.UseCollation(collation: collName);
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseNpgsql("Host=localhost;Database=BlogDb;Username=YourUsername;Password=YourPassword");
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            base.ConfigureConventions(configurationBuilder);
+
+            configurationBuilder.Properties<string>().UseCollation(collName);
         }
     }
 
@@ -34,6 +40,8 @@ namespace EfCoreCollationBug
     {
         public int BlogId { get; set; }
         public string Url { get; set; }
+        public string Address { get; set; }
+        public string Name { get; set; }
         public List<Post> Posts { get; set; } = new();
     }
 
