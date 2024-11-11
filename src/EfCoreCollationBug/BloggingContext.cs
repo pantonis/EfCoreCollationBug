97ce9a2d310e03f9d9e0add784f9fe321c64b657
pantonis <antonischa@gmail.com>
2024-11-11 11:13:04 +0200

    Initial commit

---
+++ src/EfCoreCollationBug/BloggingContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCoreCollationBug
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            string collName = """en_US.utf8_ci""";
+            
+            modelBuilder.HasCollation(name: collName, locale: "en_US.utf8", provider: "icu", deterministic: false);
+            modelBuilder.UseCollation(collation: collName);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Database=BlogDb;Username=YourUsername;Password=YourPassword");
+        }
+    }
+
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+        public List<Post> Posts { get; set; } = new();
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+
+}
